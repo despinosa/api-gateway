d134ce258bd40e100c910094fe4ba3f35eb4725f
Daniel Espinosa-Galindo <supern4ut@gmail.com>
2024-04-22 16:29:41 -0600

    Initial working version

--- api/app.py
+++ api/app.py
@@ -1,30 +1,194 @@
+import collections
+import concurrent.futures
+import functools
+import json
+import logging
+import operator
+import os
+import pygtrie
+import signal
+import threading
+import time
+
+from typing import Any, Optional
+from collections.abc import MutableMapping
+
 import flask
-from celery import states
-from worker import celery
+import celery.states
+import kombu
+import kombu.mixins
+import rabbitmq_pika_flask as rpf
+import requests
 
-dev_mode = True
-app = flask.Flask(__name__)
+import worker
+
+MQ_URL = os.environ.get('MQ_URL', 'amqp://guest:guest@localhost:5672/0')
+MQ_EXCHANGE = os.environ.get('MQ_EXCHANGE', 'ratelimiting')
+API_HOST = os.environ.get('API_HOST', 'api.github.com')
+API_PROTOCOL = os.environ.get('API_PROTOCOL', 'https')
+API_BASE = f'{API_PROTOCOL}://{API_HOST}'
+
+dev_mode: bool = True
+app: flask.Flask = flask.Flask(__name__)
+rabbit: rpf.RabbitMQ = rpf.RabbitMQ()
+
+app.logger.setLevel(logging.INFO)
+app.config['MQ_URL'] = MQ_URL
+app.config['MQ_EXCHANGE'] = MQ_EXCHANGE
+time.sleep(3)
+rabbit.init_app(app, '', body_parser=json.loads)
 
 
-@app.route('/add/<int:param1>/<int:param2>')
-def add(param1: int, param2: int) -> str:
-    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
-    response = f"<a href='{flask.url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
-    return response
+class RateLimitExceeded(RuntimeError):
+    def __init__(self, path='', ip_address='0.0.0.0'):
+        self._path = path
+        self._ip_address = ip_address
+
+    def __str__(self):
+        if self._ip_address == '0.0.0.0':
+            return f'Too many requests from any location to path "{self._path}"'
+        else:
+            return (f'Too many requests from {"IP " + self._ip_address}'
+                    f'to path "{self._path}"')
 
 
-@app.route('/check/<string:task_id>')
-def check_task(task_id: str) -> str:
-    res = celery.AsyncResult(task_id)
-    if res.state == states.PENDING:
-        return res.state
+class ThrottleRegistry:
+    _data: MutableMapping[str, set[str]] = pygtrie.StringTrie({'/': set()})
+
+    def __init__(self):
+        self._lock = threading.Lock()
+    
+    def set_throttled(prefix: str, ip_address: str) -> None:
+        with self._lock:
+            if prefix not in self._data:
+                self._data[prefix] = set()
+            self._data[prefix].add(ip)
+
+    def unset_throttled(prefix: str, ip_address: str) -> None:
+        with self._lock:
+            if prefix not in self._data:
+                self._data[prefix] = set()
+            self._data[prefix].remove(ip_address)
+
+    def is_throttled(self, prefix: str, ip_address: str) -> bool:
+        with self._lock:
+            return ip_address in self._data.longest_prefix(prefix) or set()
+
+registry = ThrottleRegistry()
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def proxy(path: str) -> flask.Response:
+    ip_address = (flask.request.remote_addr if flask.request.remote_addr
+                  else '0.0.0.0')
+    if registry.is_throttled(path, ip_address):
+        raise RateLimitExceeded()
+    worker.celery.send_task('tasks.hit', args=[path, ip_address])
+
+    proxied = requests.request(  # ref. https://stackoverflow.com/a/36601467/248616
+        method=flask.request.method,
+        url=flask.request.url.replace(flask.request.host_url, f'{API_BASE}/'),
+        headers={k: v for k, v in flask.request.headers
+                 if k.lower() != 'host'}, # exclude 'host' header
+        data=flask.request.get_data(),
+        cookies=flask.request.cookies,
+        allow_redirects=False,
+    )
+
+    # region exlcude some keys in :proxied response
+    excluded_headers = ['content-encoding', 'content-length',
+                        'transfer-encoding', 'connection']
+    # NOTE: we here exclude all "hop-by-hop headers" defined by RFC 2616 section
+    #       13.5.1 ref. https://www.rfc-editor.org/rfc/rfc2616#section-13.5.1
+    headers = [(k, v) for k, v in proxied.raw.headers.items()
+               if k.lower() not in excluded_headers]
+
+    return flask.Response(proxied.content, proxied.status_code, headers)
+
+@app.route('/_/free/<path:subpath>', methods=['PUT'])
+def throttle_assignment(subpath: str) -> flask.Response:
+    if flask.request.json:
+        if type(flask.request.json) is not list and type(flask.request.json[0]) is int:
+            global registry
+            for ip_address in ip_addresses:
+                registry.set_throttled(subpath, ip_address)
+        else:
+            raise ValueError(f"Unexpected content {flask.request.json}")
     else:
-        return str(res.result)
+        registry.unset_throttled(subpath, 0)
 
+@app.route('/_/free/<path:subpath>', methods=['PUT'])
+def free_assignment(subpath: str) -> flask.Response:
+    if flask.request.json:
+        if (type(flask.request.json) is list
+                and type(flask.request.json[0]) is int):
+            global registry
+            for ip_address in ip_addresses:
+                registry.unset_throttled(subpath, ip_address)
+        else:
+            raise ValueError(f"Unexpected content {flask.request.json}")
+    else:
+        registry.unset_throttled(subpath, 0)
 
-@app.route('/health_check')
+@app.route('/_/healthz')
 def health_check() -> flask.Response:
     return flask.jsonify("OK")
+
+@app.errorhandler(ValueError)
+def handle_value_error(error):
+    response = flask.jsonify({'status': 'fail',
+                              'message': str(error)})
+    response.status_code = 400
+    return response
+
+@app.errorhandler(RateLimitExceeded)
+def handle_rate_limited(exception):
+    response = flask.jsonify({'status': 'fail',
+                              'message': str(exception)})
+    response.status_code = 429
+    return response
+
+@rabbit.queue(routing_key='freed')
+def freed_event(routing_key: str, body: dict[str, Any]) -> None:
+    if 'status' in body:
+        if body['status'] != 'success':
+            app.logger.error('%s event with bad status %s: "%s"', routing_key,
+                             body['status'], body)
+    else:
+        app.logger.error('Malformed %s event: %s', routing_key, body)
+    if 'data' in body:
+        app.logger.debug('Freeing path "%s" for IP %s',
+                         body['data'].get('path', ''),
+                         body['data'].get('ipAddress', ''))
+        global registry
+        registry.unset_throttled(body['data'].get('path', ''),
+                                 body['data'].get('ipAddress', ''))
+        app.logger.warning('Freed path "%s" for IP %s',
+                           body['data'].get('path', ''),
+                           body['data'].get('ipAddress', ''))
+    else:
+        app.logger.error('Malformed %s event: %s', routing_key, body)
+
+@rabbit.queue(routing_key='throttled')
+def throttled_event(routing_key: str, body: dict[str, Any]) -> None:
+    if 'status' in body:
+        if body['status'] != 'success':
+            app.logger.error('%s event with bad status %s: "%s"', routing_key,
+                             body['status'], body)
+    else:
+        app.logger.error('Malformed %s event: %s', routing_key, body)
+    if 'data' in body:
+        app.logger.debug('Throttling path "%s" for IP %s',
+                         body['data'].get('path', ''),
+                         body['data'].get('ipAddress', ''))
+        global registry
+        registry.set_throttled(body['data'].get('path', ''),
+                               body['data'].get('ipAddress', ''))
+        app.logger.warning('Throttled path "%s" for IP %s',
+                           body['data'].get('path', ''),
+                           body['data'].get('ipAddress', ''))
+    else:
+        app.logger.error('Malformed %s event: %s', routing_key, body)
 
 
 if __name__ == '__main__':
--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -1,17 +1,107 @@
+import copy
+import functools
+import json
 import os
-import time
+import datetime
+
+from collections.abc import MutableMapping
+from typing import Dict, Tuple
 
 import celery
+import redis
+import redis.lock
+import pika
+import pygtrie
 
 
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
+# CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'amqp://guest:guest@localhost:5672/0')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
+MQ_URL = os.environ.get('MQ_URL', 'amqp://guest:guest@localhost:5672/0')
+MQ_EXCHANGE = os.environ.get('MQ_EXCHANGE', 'ratelimiting')
 
-deferred = celery.Celery(
-        'tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+scheduler = celery.Celery('tasks', broker=CELERY_BROKER_URL,
+                         backend=CELERY_RESULT_BACKEND)
+
+def mutex(function=None, key='', timeout=None):
+    @functools.wraps(function)
+    def decorator(function_runner):
+        @functools.wraps(function_runner)
+        def worker(*args, **kwargs):
+            return_value = None
+            # lock = celery.backend.redis.client.lock(key, timeout=timeout)
+            lock = redis.lock.Lock(scheduler.backend.client, key, timeout=timeout)
+            with lock:
+                return_value = function_runner(*args, **kwargs)
+            return return_value
+        return worker
+    return decorator if function is None else decorator(function)
 
 
-@deferred.task(name='tasks.add')
-def add(x: int, y: int) -> int:
-    time.sleep(5)
-    return x + y
+class RateLimiterRegistry:
+    blocklist: dict[str, set[int]] = {}
+    _state: MutableMapping[str, dict[int, int]] = pygtrie.StringTrie()
+    _default: MutableMapping[str, dict[int, int]] = pygtrie.StringTrie(
+        {'/': {'152.152.152.152': 1000},
+         '/categories/': {0: 1000},
+         '/items/': {'152.152.152.152': 10}}
+    )
+
+    def __init__(self):
+        self.reset()
+
+    @mutex(timeout=3)
+    def evaluate(self, path: str, ip_address: int, new_hits=1) -> bool:
+        substate = self._state.longest_prefix(path).value or self._state['/']
+        lookup = ip_address if ip_address in substate else 0
+        if lookup not in substate:
+            return False
+        substate[lookup] -= new_hits
+        if substate[lookup] <= 0:
+            if path not in self.blocklist:
+                self.blocklist[path] = set()
+            self.blocklist[path].add(ip_address)
+        return substate[lookup] <= 0
+    
+    @mutex(timeout=60)
+    def reset(self) -> None:
+        self._state = copy.deepcopy(self._default)
+        self.blocklist = {}
+
+
+registry = RateLimiterRegistry()
+
+@scheduler.task(name='tasks.hit')
+def hit(path: str, ip_address: str) -> Dict[str, int | str]:
+    if registry.evaluate(path, ip_address):
+        with pika.BlockingConnection(
+                parameters=pika.URLParameters(MQ_URL)) as connection:
+            with connection.channel() as channel:
+                channel.basic_publish(
+                    MQ_EXCHANGE, 'throttled',
+                    json.dumps({'data': {'path': path,
+                                         'ipAddress': ip_address}})
+                )
+    return {'timestamp': int(datetime.datetime.now().timestamp()),
+            'path': path,
+            'ip_address': ip_address}
+
+@scheduler.task(name='tasks.cycle', ignore_result=True)
+def cycle() -> None:
+    newly_allowlisted = copy.deepcopy(registry.blocklist)
+    registry.reset()
+    for path, ip_addresses in newly_allowlisted:
+        for ip_address in ip_addresses:
+            with pika.BlockingConnection(
+                    parameters=pika.URLParameters(MQ_URL)) as connection:
+                with connection.channel() as channel:
+                    channel.basic_publish(
+                        MQ_EXCHANGE, 'freed',
+                        json.dumps({'data': {'path': path,
+                                             'ipAddress': ip_address}})
+                    )
+
+@scheduler.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(60.0, cycle.s())
+
